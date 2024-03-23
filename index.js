54407a8645a81aa3ff7c422d727b87701d62295c
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2024-03-23 21:01:04 +0100

    implemented routers for users

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import {userRouter}  from './users/users.js';
+
 
 const port = 8000;
 const app = express();
@@ -6,6 +8,8 @@ const app = express();
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
