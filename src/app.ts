90a16a6271960390998504409227ca0e58803aac
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2024-04-02 11:33:13 +0200

    implemented exeptionFilters for Errors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { Server } from "http";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
+import { ExeptionFiltres } from "./errors/exeption.filter";
 
 export class App {
   app: Express;
@@ -9,20 +10,31 @@ export class App {
   port: number;
   logger: LoggerService;
   userController: UserController;
+  exeptionFilter: ExeptionFiltres;
 
-  constructor(logger: LoggerService, userController: UserController) {
+  constructor(
+    logger: LoggerService,
+    userController: UserController,
+    exeptionFilter: ExeptionFiltres
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
     this.userController = userController;
+    this.exeptionFilter = exeptionFilter;
   }
 
   useRoutes() {
     this.app.use("/users", this.userController.router);
   }
 
+  useExeptionFiltres() {
+    this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter));
+  }
+
   public async init() {
     this.useRoutes();
+    this.useExeptionFiltres();
     this.server = this.app.listen(this.port);
     this.logger.log(`Server is running on http://localhost:${this.port}`);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { App } from "./app";
+import { ExeptionFiltres } from "./errors/exeption.filter";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
 
 async function bootstrap() {
-    const logger = new LoggerService;
-    const app = new App(logger, new UserController(logger));
-    await app.init();
+  const logger = new LoggerService();
+  const app = new App(
+    logger,
+    new UserController(logger),
+    new ExeptionFiltres(logger)
+  );
+  await app.init();
 }
 
 bootstrap();
